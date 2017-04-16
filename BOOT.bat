50cbef1b078e93e6dd9e5afbc380487165765d3c
Nikolas Karinja <nikobikoliko@gmail.com>
2017-04-15 21:16:27 -0700

    Add files via upload

---
+++ BOOT.bat
@@ -0,0 +1,15 @@
+@echo off
+echo.
+echo Loading Mouse Driver...
+echo.
+MOUSE.COM
+echo.
+echo Loading System Basic...
+echo.
+echo Checking Drives for errors...
+echo.
+CDSCAN.EXE
+echo.
+echo Loading Avix-OS 2.0...
+echo.
+SYS.EXE
